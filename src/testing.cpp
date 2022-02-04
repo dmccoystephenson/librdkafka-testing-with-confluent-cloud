e0f6a3d30398cbaec32f77308ee6f74f1d01b051
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-04 10:32:28 -0700

    Added librdkafka as a git submodule.

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,8 +1,17 @@
 #include <string>
 #include <iostream>
+#include "rdkafkacpp.h";
 
 using namespace std;
 
 int main() {
-    
+    log("Executing program.");
+
+    // TODO: implement
+
+    log("Program finished executing.");
+}
+
+void log(string message) {
+    cout << message << endl;
 }
