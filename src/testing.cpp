3d16e40e3cf67c2ce48c42be40260f9ae9760e75
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-07 08:07:51 -0700

    Replaced hard-coded values with environment variables and added comments.

--- src/testing.cpp
+++ src/testing.cpp
@@ -7,20 +7,20 @@ using namespace std;
 int main() {
     cout << "Executing program." << endl;
     
+    // prepare variables
     string brokers = "pkc-43n10.us-central1.gcp.confluent.cloud:9092";
     string topic = "topic.Asn1DecoderInput";
+    string username = getenv("CONFLUENT_KEY");
+    string password = getenv("CONFLUENT_SECRET");
+    string errorString = "";
 
+    // configuration
     cout << "Creating configuration" << endl;
     RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
 
-    string errorString;
-
     conf->set("bootstrap.servers", brokers, errorString);
 
-    cout << "Setting up Confluent Cloud configuration key/value pairs." << std::endl; // DEBUG
-
-    string username = "K4O367P4UIH3ZPJ7";
-    string password = "QSWUoQAMxfFZqMjgqEmk18qcL6NMiLjF83X+7+8ScWrT8lzh7DqCMiDeSnnsWqOF";
+    cout << "Setting up Confluent Cloud configuration key/value pairs." << endl;
 
     conf->set("security.protocol", "SASL_SSL", errorString);
     conf->set("sasl.mechanisms", "PLAIN", errorString);
@@ -28,7 +28,7 @@ int main() {
     conf->set("sasl.password", password.c_str(), errorString);
     conf->set("session.timeout.ms", "45000", errorString);
 
-    std::cout << "Finished setting up Confluent Cloud configuration key/value pairs." << std::endl;
+    cout << "Finished setting up Confluent Cloud configuration key/value pairs." << endl;
 
     // TODO: set delivery port callback?
 
@@ -36,13 +36,14 @@ int main() {
     cout << "Creating producer instance." << endl;
     RdKafka::Producer *producer = RdKafka::Producer::create(conf, errorString);
 
+    // delete config
     cout << "Deleting configuration." << endl;
     delete conf;
 
     string message = "test";
 
+    // produce
     cout << "Producing." << endl;
-
     RdKafka::ErrorCode errorCode = producer->produce(
                         /* Topic name */
                         topic,
@@ -61,6 +62,7 @@ int main() {
                         /* Per-message opaque value passed to delivery report */
                         NULL);
         
+    // error checking
     if (errorCode != RdKafka::ERR_NO_ERROR) {
         cout << "Failed to produce to topic " << topic << "." << endl;
     }
@@ -68,9 +70,11 @@ int main() {
         cout << "Enqueued message (" << message.size() << "bytes) for topic " << topic << endl;
     }
     
+    // flush
     cout << "Flushing final messages..." << std::endl;
     producer->flush(10*1000);
 
+    // delete producer
     cout << "Deleting producer." << endl;
     delete producer;
 
